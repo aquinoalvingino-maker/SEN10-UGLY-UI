400f88908071b25bd66c98375b377975d04bc759
aquinoalvingino-maker <aquino.alvingino@student.auf.edu.ph>
2025-11-05 13:17:28 +0800

    failed tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
+// eslint.config.js (ESM version)
 import js from "@eslint/js";
 import globals from "globals";
+import jestPlugin from "eslint-plugin-jest";
 
 export default [
   js.configs.recommended,
@@ -7,14 +9,14 @@ export default [
     files: ["**/*.js"],
     languageOptions: {
       globals: {
-        ...globals.browser, // if you have browser code
+        ...globals.browser,
       },
     },
   },
   {
     files: ["**/*.test.js", "**/tests/**/*.js"],
     plugins: {
-      jest: require("eslint-plugin-jest"),
+      jest: jestPlugin,
     },
     languageOptions: {
       globals: {
