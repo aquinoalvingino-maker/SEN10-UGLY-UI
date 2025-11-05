40eb1d308660784e248b09d6fca6cd17eddcb1d8
aquinoalvingino-maker <aquino.alvingino@student.auf.edu.ph>
2025-11-05 10:37:52 +0800

    UPDATED

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Welcome to the Ugly UI Page!");
+
+// Example of a simple interactive element
+document.querySelector('h1').addEventListener('click', function() {
+    alert('You clicked the header!');
+});
