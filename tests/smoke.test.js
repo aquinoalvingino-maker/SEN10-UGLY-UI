10b277dcb3c3d36cfb8c0fef825a13a40fd81b89
aquinoalvingino-maker <aquino.alvingino@student.auf.edu.ph>
2025-11-05 13:12:22 +0800

    failed tests

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -1,12 +1,13 @@
-// smoke.test.js
+/* eslint-env jest */
+
 describe("Basic Smoke Tests", () => {
   test("This test should PASS", () => {
     const sum = 2 + 3;
-    expect(sum).toBe(5); // ✅ Passes
+    expect(sum).toBe(5);
   });
 
   test("This test should FAIL", () => {
     const truth = false;
-    expect(truth).toBe(true); // ❌ Fails intentionally
+    expect(truth).toBe(true);
   });
 });
